5a047fdc82d65c328889fa120fe7566d60b3f79e
Zalfauzi__ <118164508+Zal77@users.noreply.github.com>
2022-12-08 16:00:48 +0700

    Delete rental-PS directory

--- rental-PS/sub.h
+++
@@ -1,7 +0,0 @@
-using namespace std;
-long sewa[5]={5000,4000,5500,7000,10000};
-long pinjam[5]={45000,35000,50000,70000,95000};
-float makanan,minuman;
-int lamaJam,lamaHari,rasa;
-float totalBayar,totalHarga1,totalHarga2,totalHarga3,totalseluruh;
-string nama,alamat,yesno,varian,enter,spasi;
